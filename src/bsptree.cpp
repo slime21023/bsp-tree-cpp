73c29598550a924ce72c15933c1f9a34a5383ccf
slime <slime21023@gmail.com>
2023-04-24 14:52:53 +0800

    first version

--- src/bsptree.cpp
+++ src/bsptree.cpp
@@ -80,8 +80,10 @@ BSPTree::BSPTree(int n_dim, int n_leaf, vector<Point> ps)
     dim = n_dim;
     leaf_size = n_leaf;
     points = ps;
+    first = nullptr;
+    second = nullptr;
 
-    if(points.size() >= leaf_size)
+    if(points.size() > leaf_size)
     {
         split();
     }
@@ -156,9 +158,125 @@ void BSPTree::split()
 
     first = new BSPTree(dim, leaf_size, fps);
     second = new BSPTree(dim, leaf_size, sps);
+    first->pivot = fp;
+    second->pivot = sp;
 }
 
 void BSPTree::insert(Point p)
 {
-    
+    bool is_leaf = (first == nullptr) && (second == nullptr);
+    if(is_leaf)
+    {
+        points.push_back(p);
+    }
+    else
+    {
+        double fd, sd;
+        fd = points_distance(p, first->pivot, dim);
+        sd = points_distance(p, second->pivot, dim);
+
+        if(fd < sd)
+        {
+            first->insert(p);
+        } 
+        else
+        {
+            second->insert(p);
+        }
+    }
+
+    if (is_leaf && points.size() > leaf_size)
+    {
+        split();
+    }
+}
+
+vector<Point> BSPTree::range_query(HRect& rect)
+{
+    bool is_leaf = (first == nullptr) && (second == nullptr);
+    vector<Point> ps;
+
+    auto close_first = [this] (Point p) -> bool {
+        double fd, sd;
+        fd = points_distance(p, first->pivot, dim);
+        sd = points_distance(p, second->pivot, dim);
+        return (fd < sd);
+    };
+
+    if (!is_leaf)
+    {
+        Point min_point, max_point;
+        for (int i = 0; i < dim; i++)
+        {
+            min_point.push_back(rect.mins[i]);
+            max_point.push_back(rect.maxs[i]);
+        }
+
+        bool min_close_first = close_first(min_point);
+        bool max_close_first = close_first(max_point);
+
+        if (min_close_first == true && max_close_first == true)
+        {
+            vector<Point> fpoints = first->range_query(rect);
+            ps.insert(ps.end(), fpoints.begin(), fpoints.end());
+        }
+        else if (min_close_first == false && max_close_first == false)
+        {
+            vector<Point> spoints = second->range_query(rect);
+            ps.insert(ps.end(), spoints.begin(), spoints.end());
+        }
+        else
+        {
+            vector<Point> fpoints = first->range_query(rect);
+            vector<Point> spoints = second->range_query(rect);
+            ps.insert(ps.end(), fpoints.begin(), fpoints.end());
+            ps.insert(ps.end(), spoints.begin(), spoints.end());
+        }
+    }
+    else
+    {
+        for (auto p : points) {
+            if (is_include(rect, p)) {
+                ps.push_back(p);
+            }
+        }
+    }
+
+    return ps;
+}
+
+Point BSPTree::point_query(Point& p)
+{
+    bool is_leaf = (first == nullptr) && (second == nullptr);
+    Point result;
+
+    auto close_first = [this] (Point p) -> bool {
+        double fd, sd;
+        fd = points_distance(p, first->pivot, dim);
+        sd = points_distance(p, second->pivot, dim);
+        return (fd < sd);
+    };
+
+    if (!is_leaf)
+    {
+        bool is_close_first = close_first(p);
+        if (is_close_first)
+        {
+            result = first->point_query(p);
+        }
+        else 
+        {
+            result = second->point_query(p);
+        }
+    }
+    else
+    {
+        for (auto op : points)
+        {
+            if(is_equal(p, op))
+                return op;
+        }
+    }
+
+    return result;
 }
